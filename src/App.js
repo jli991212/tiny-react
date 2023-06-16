b3a2608c4e7667263cc51f0f85185fa8f1fb775c
jli991212 <jiadingl18@gmail.com>
2023-06-16 12:09:35 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
+import {Routes,Route,Link} from 'react-router-dom';
 import './App.css';
 import Contact from './Components/Contact';
 function App() {
